bcebd504539fab91c3069e9a42654d4303be0e18
mathias260696 <mathias.standaert@hotmail.com>
2017-10-07 13:16:38 +0200

    clicksounds

--- views/assets/js/script.js
+++ views/assets/js/script.js
@@ -3,15 +3,20 @@
  */
 var singleplayer = function (e) {
     e.preventDefault();
+    soundClick();
     $('#mainMenu').addClass("hidden");
     $('#levelpicker').removeClass("hidden");
     $('video').prop('muted', true);
 };
 var startGame = function (e) {
     e.preventDefault();
+    soundClick();
     $('#levelpicker').addClass("hidden");
     $('#game').removeClass("hidden");
-}
+};
+var soundClick = function () {
+    new Audio("/views/assets/images/Cursor.wav").play();
+};
 $(document).ready(function () {
     $('#singleplayer').on('click',singleplayer);
     $('.levelx.active').on('click',startGame);
